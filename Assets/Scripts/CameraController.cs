674364e3639dd6b3d7ed7e420993a0a459ea8dc4
sepehr ansaripour <pehrans@gmail.com>
2018-01-11 22:33:41 -0500

    Did some testing with boundaries and improved scripts

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public float zVal;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = Plane.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = Plane.position;
+		newPos.z = zVal;;
+		transform.position = newPos;
+	}
+}
--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class Plane : MonoBehaviour {
 	Rigidbody2D rigidBody2D;
 	float index;
-	float ampY;
+	public float ampY;
+	public float speed;
+	public static Vector3 position;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody2D = this.gameObject.GetComponent<Rigidbody2D> ();
 		index = 0;
-		ampY = 0.25f;
-		rigidBody2D.AddForce (new Vector2 (100, 0));
+		rigidBody2D.AddForce (new Vector2 (speed, 0));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		index += Time.deltaTime;
-		Vector2 position = rigidBody2D.position;
+		position = transform.position;
 		rigidBody2D.position = new Vector2(position.x, ampY * Mathf.Cos (2*index));
 	}
 }
